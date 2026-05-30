54a201dddba01189adde8c837304b354d8978f98
benchimol2305 <cesarbenchimoldavila@gmail.com>
2026-05-29 20:44:02 -0400

    se crea la  estructura visual y formulario del componente Login

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,4 +1,4 @@
-useState } from 'react';
+import React, {useState } from 'react';
 
 export default function Login({ onLoginSuccess }) {
   const [username, setUsername] = useState('');
@@ -12,3 +12,22 @@ export default function Login({ onLoginSuccess }) {
       onLoginSuccess();
     } else {
       setError('Credenciales incorrectas. Intenta con admin / 1234');
+    }
+};
+
+return (
+    <div style={{ maxWidth: '300px', margin: '100px auto', padding: '20px', border: '1px solid #ccc', borderRadius: '8px' }}>
+      <h2>Iniciar Sesión</h2>
+      <form onSubmit={handleSubmit}>
+        <div style={{ marginBottom: '10px' }}>
+          <label>Usuario: </label>
+          <input 
+            type="text" 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)} 
+            style={{ width: '100%', padding: '6px' }}
+            required 
+          />
+        </div>
+        <div style={{ marginBottom: '10px' }}>
+          <label>Contraseña: </label>
